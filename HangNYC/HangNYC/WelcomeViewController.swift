f0e446b831677c865320c5f6a992851b70efe54d
JohanDelao <johandelao10@gmail.com>
2024-02-17 11:23:23 -0500

    actually programming swift

--- HangNYC/HangNYC/WelcomeViewController.swift
+++ HangNYC/HangNYC/WelcomeViewController.swift
@@ -9,22 +9,49 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var test: UILabel!
+//    @IBOutlet weak var test: UILabel!
+    let testButton = UIButton()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        test.text = "hello world"
+//        test.text = "hello world"
+        testTapped()
+        view.backgroundColor = .systemBackground
+        title = "Welcome"
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
 
-    @IBAction func testTapped(_ sender: Any) {
-        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
-        let eventSetup = storyBoard.instantiateViewController(withIdentifier: "eventSetup") as! EventSetupViewController
+    func testTapped() {
+        view.addSubview(testButton)
+        testButton.configuration = .filled()
+        testButton.configuration?.baseBackgroundColor = .systemGreen
+        testButton.configuration?.title = "Test"
         
-        let navigationController = UINavigationController(rootViewController: eventSetup)
-        navigationController.modalPresentationStyle = .fullScreen
-        self.present(navigationController, animated: true, completion: nil)
+        testButton.translatesAutoresizingMaskIntoConstraints = false // must do this for every UI element
+        
+        testButton.addTarget(self, action: #selector(goToNextScreen), for: .touchUpInside)
+        
+        NSLayoutConstraint.activate([
+            testButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            testButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            testButton.widthAnchor.constraint(equalToConstant: 200),
+            testButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+//        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+//        let eventSetup = storyBoard.instantiateViewController(withIdentifier: "eventSetup") as! EventSetupViewController
+        
+//        let navigationController = UINavigationController(rootViewController: eventSetup)
+//        navigationController.modalPresentationStyle = .fullScreen
+//        self.present(navigationController, animated: true, completion: nil)
+    }
+    
+    @objc func goToNextScreen(){
+        let nextScreen = EventSetupViewController()
+        nextScreen.title = "Event Setup"
+        navigationController?.pushViewController(nextScreen, animated: true)
     }
     
 }
