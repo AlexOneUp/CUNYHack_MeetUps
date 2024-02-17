3cbd13545af138af5a1d7771d3fd7ccfabef5fdb
Alex R <ruanalex11235@gmail.com>
2024-02-17 14:40:29 -0500

    Changing up Backend structure
    Changing backend structure, added more endpoint logic

--- Server/app.py
+++ Server/app.py
@@ -1,6 +1,14 @@
 from flask import Flask, jsonify, request
+from dotenv import load_dotenv
+import googlemaps
+import json
+import requests
+import os
 
 app = Flask(__name__)
+
+# Google Maps API Key
+gmaps = googlemaps.Client(key=os.getenv('googlemapsAPIKey'))
 
 @app.route('/')
 def home():
@@ -9,33 +17,42 @@ def home():
 @app.route('/data', methods=['GET'])
 def get_data():
     # Here you can fetch and return data
-    data = {"message": "This is some data from the Flask backend."}
+    data = {
+        "message": "This is some data from the Flask backend."
+        }
     return jsonify(data)
 
 
 # This route will receive a POST request with a JSON body of Geocoding data
 # Algorithm will figure out the mid point of the number of users and return the location
-@app.route('/post_location_finder', methods=['POST'])
-def post_location_finder():
+
+@app.route('/post_find_user_geocodes', methods=['POST'])
+def post_find_user_geocodes():
     data = request.json
-    
-    # Extracting the 'addresses' and 'preferences' from the JSON payload
+
     addresses = data.get('addresses', [])
-    preferences = data.get('preferences', [])
+    if not addresses:
+        return jsonify({"error": "No addresses provided"}), 400
+
+    geocoded_addresses = []
+
+    for address in addresses:
+        # Geocoding an address
+        geocode_result = gmaps.geocode(address)
+        if geocode_result:
+            geocoded_addresses.append(geocode_result[0]['geometry']['location'])
+        else:
+            geocoded_addresses.append({"error": "Unable to geocode address", "address": address})
+    response = requests.post(url, json=data)
+    return jsonify(geocoded_addresses), 200
+
+    # Extracting the 'addresses' and 'preferences' from the JSON payload
+
 
     # Here you can add processing logic based on the received addresses and preferences
     # Calculate the mid point of the addresses and return the location
     
     
-    # Example: just echoing back the received data
-    response_data = {
-        "message": "Data processed successfully",
-        "received_addresses": addresses,
-        "received_preferences": preferences
-    }
-    print(response_data)
-    # Returning a JSON response
-    return jsonify(response_data), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
